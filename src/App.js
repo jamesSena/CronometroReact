e1172ced48ddeb753a7a15e246b942b6c14b4091
Jamesson Sales de Sena <jsdesena@interaxa.com>
2019-10-10 16:25:36 -0300

    Trabalhando com states no botao

--- src/App.js
+++ src/App.js
@@ -4,15 +4,49 @@ import './estilo.css';
 class App extends Component{
   constructor(props){
     super(props);
+    this.state={
+      numero:0,
+      botao:'Vai'
+    }
+    this.timer=null;
+    this.vai = this.vai.bind(this);
+    this.limpar = this.limpar.bind(this);
+
   }
+vai(){
+  let state = this.state;
+
+  if(this.timer !== null){
+    clearInterval(this.timer);
+    this.timer=null;
+    state.botao='Vai';
+  }else{
+    this.timer = setInterval(()=>{
+      let state = this.state;
+      state.numero += 0.1;
+      this.setState(state);
+   },100);
+   state.botao='Pausar';
+  }
+  this.setState(state);
+}
+limpar(){
+  if(this.timer !== null){
+    clearInterval(this.timer);
+    this.timer=null;
+  }
+  let state = this.state;
+  state.numero = 0;
+  this.setState(state);
+}
   render(){
     return (
       <div className="container">
           <img className="img" src={require('./assets/cronometro.png')} />
-          <a className="timer">0.0</a>
+          <a className="timer">{this.state.numero.toFixed(1)}</a>
           <div className="areaBtn">
-            <a className="botao">Vai</a>
-            <a className="botao">Limpar</a>
+            <a className="botao" onClick={this.vai}>{this.state.botao}</a>
+            <a className="botao" onClick={this.limpar}>Limpar</a>
           </div>
       </div>
     );
