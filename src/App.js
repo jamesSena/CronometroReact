99fa5244986f6921326957cc54f0c09431acb961
Jamesson Sales de Sena <jsdesena@interaxa.com>
2019-10-10 16:14:31 -0300

    Tratamento do CSS. Alterando APP.js para component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './estilo.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      <div className="container">
+          <img className="img" src={require('./assets/cronometro.png')} />
+          <a className="timer">0.0</a>
+          <div className="areaBtn">
+            <a className="botao">Vai</a>
+            <a className="botao">Limpar</a>
+          </div>
+      </div>
+    );
+  }
 }
-
 export default App;
